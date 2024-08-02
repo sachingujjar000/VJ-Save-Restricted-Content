c3cad415d40dd3f69d766ef19df9e12f0ed9e25a
sachingujjar000 <122327502+sachingujjar000@users.noreply.github.com>
2024-08-02 16:21:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "26549440"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "9a4aba787f86b42205eb7e735d3eba5e")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://sachingurjar10122004:<LwQ6JZCHGukQCbL0>@sachinbot.e0uy8bb.mongodb.net/?retryWrites=true&w=majority&appName=Sachinbot")
